7b766045a285a4564f0cbec3e669a035a5cbd451
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2024-04-07 21:08:19 +0530

    feat: add arrows

--- keyboards/sofle/keymaps/Sushrit_Lawliet/keymap.c
+++ keyboards/sofle/keymaps/Sushrit_Lawliet/keymap.c
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   [  |  ]   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ESC  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * | ESC  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------| LEFT | DOWN |  UP  | RIGHT|   ;  |  '   |
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT(
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,  KC_MINS,  KC_EQL,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_LBRC,  KC_RBRC,
-  KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_LEFT,    KC_DOWN,    KC_UP,    KC_RIGHT, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
                  KC_LCTL,KC_LGUI,KC_LALT, TL_LOWR, KC_SPC,      KC_ENT,  TL_UPPR, KC_DEL, KC_MENU, KC_RGUI
 ),
@@ -309,7 +309,7 @@ static void render_status(void) {
             oled_write("Base ", false);
             break;
         case _COLEMAK:
-            oled_write("Colemak", false);
+            oled_write("Colmk", false);
             break;
         case _RAISE:
             oled_write("Raise", false);
